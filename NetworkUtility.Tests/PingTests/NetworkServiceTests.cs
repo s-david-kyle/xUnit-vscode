fe5de73ccc82313096a08898abacaa427f01a1cc
S. David Kyle <sdavidkyle@gmail.com>
2024-04-25 15:09:43 -0700

    Adding Fluent tests

---
+++ NetworkUtility.Tests/PingTests/NetworkServiceTests.cs
@@ -0,0 +1,67 @@
+﻿using FluentAssertions;
+using FluentAssertions.Extensions;
+
+namespace NetworkUtility.Tests;
+
+public class NetworkServiceTests
+{
+    private readonly NetworkService _networkService;
+
+    public NetworkServiceTests()
+    {
+        //SUT
+        _networkService = new NetworkService();
+    }
+    [Fact]
+    public void NetworkService_SendPing_ReturnsString()
+    {
+        // Arrange
+        // Act
+        var result = _networkService.SendPing();
+
+        // Assert
+        result.Should().NotBeNullOrWhiteSpace();
+        result.Should().Be("Success: Ping Sent!");
+        result.Should().Contain("Ping");
+    }
+
+    [Theory]
+    [InlineData(1, 1, 2)]
+    [InlineData(2, 3, 5)]
+    public void NetworkService_PingTimeout_ReturnsSum(int a, int b, int expected)
+    {
+        // Arrange
+        // Act
+        var result = _networkService.PingTimeout(a, b);
+
+        // Assert
+        result.Should().Be(expected);
+    }
+
+    [Fact]
+    public void NetworkService_LastPingDate_ReturnsDateTime()
+    {
+        // Arrange
+        // Act
+        var result = _networkService.LastPingDate();
+
+        // Assert
+        result.Should().BeAfter(1.January(2022));
+        result.Should().BeBefore(1.January(2033));
+    }
+
+    [Fact]
+    public void GetPingOptionsTest()
+    {
+        // Arrange
+        // Act
+        var result = _networkService.GetPingOptions();
+
+        // Assert
+        result.Should().NotBeNull();
+        result.DontFragment.Should().BeTrue();
+        result.Ttl.Should().Be(1);
+    }
+
+
+}
---
+++ NetworkUtility/Ping/NetworkService.cs
@@ -0,0 +1,31 @@
+﻿using System.Net.NetworkInformation;
+
+namespace NetworkUtility;
+
+public class NetworkService
+{
+    public string SendPing()
+    {
+        return "Success: Ping Sent!";
+    }
+
+    public int PingTimeout(int a, int b)
+    {
+        return a + b;
+    }
+
+    public DateTime LastPingDate()
+    {
+        return DateTime.Now;
+    }
+
+    public PingOptions GetPingOptions()
+    {
+        return new PingOptions()
+        {
+            DontFragment = true,
+            Ttl = 1
+        };
+
+    }
+}
